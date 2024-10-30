799768b73c2e351a4b7283a8726c5baa0362aeaa
cdrice26 <156816748+cdrice26@users.noreply.github.com>
2024-10-30 15:57:54 -0400

    added mysql connection

--- server/main.py
+++ server/main.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+# MySQL configurations
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = ""
+app.config["MYSQL_DB"] = "sharc"
+
+mysql = MySQL(app)
 
 
 @app.route("/")
@@ -8,5 +17,14 @@ def hello():
     return "Hello, World!"
 
 
+@app.route("/data")
+def get_data():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM users")
+    results = cur.fetchall()
+    cur.close()
+    return jsonify(results)
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=3000)
