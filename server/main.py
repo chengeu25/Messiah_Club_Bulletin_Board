d92c38297b270dd72f73b9f3844b406ce6c9c743
Matthew Merlo <mdmerlo1@gmail.com>
2024-11-06 18:38:55 -0500

    password reset mk1

--- server/main.py
+++ server/main.py
@@ -88,6 +88,46 @@ def signup():
     cur.close()
     return jsonify(results)
 
+@app.route("/api/passwordReset", methods=["POST"])
+def reset_password():
+    data = request.json
+    cur = mysql.connection.cursor()
+    print(data["emailRequest"])
+
+    # Retrieve the hashed password from the database
+    cur.execute(
+        """Select pwd1, pwd2, email_verified
+                FROM users
+                WHERE email = %s
+                    AND is_active = 1""",
+        (data["emailRequest"],),
+    )
+    result = cur.fetchone()
+
+    # If no matching email is found, return an error
+    if result is None:
+        return jsonify({"error": "Invalid email or password"}), 401
+
+    # Verify the provided current password against the hashed password
+    hashed_password = result[0]
+    if not check_password_hash(hashed_password, data["oldPassword"]):
+        return jsonify({"error": "Invalid password"}), 401
+
+    # Set the user ID and activity timestamp in the session
+    session["user_id"] = data["emailRequest"]
+    session["last_activity"] = datetime.now(timezone.utc)
+
+    # Check if email is verified
+    is_email_verified = result[2]
+    if not is_email_verified == 1:
+        return jsonify({"error": "Email not verified"}), 401
+
+    # Cascade passwords
+    cur.execute("""UPDATE users
+                SET pwd3 = %s, pwd2 = %s, pwd1 = %s
+                WHERE email = %s""",
+        (result[1], result[0], data['newPassword'], data['emailRequset']))
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
