6cdd6f99a4c026cb055a06a22636349396bc1c72
Matthew Merlo <mdmerlo1@gmail.com>
2024-12-08 16:23:59 -0500

    Faculty Admin button now appears without needing refresh

--- client/mcbb-frontend/src/components/sidebar/Sidebar.component.tsx
+++ client/mcbb-frontend/src/components/sidebar/Sidebar.component.tsx
@@ -1,43 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SidebarButton from './SidebarButton.component';
 import { FaCalendar, FaUsers, FaChalkboardTeacher } from 'react-icons/fa';
 import { RiCompassDiscoverFill } from 'react-icons/ri';
 import checkUser from '../../helper/checkUser';
 import { UserType as User } from '../../types/databaseTypes';
 
+const Sidebar = () => {
+  const [buttonList, setButtonList] = useState([
+    {
+      text: 'Discover',
+      icon: <RiCompassDiscoverFill />,
+      route: '/dashboard/home',
+    },
+    { text: 'Calendar', icon: <FaCalendar />, route: '/dashboard/calendar' },
+    { text: 'Clubs', icon: <FaUsers />, route: '/dashboard/clubs' },
+  ]);
 
-const buttonList = [
-  {
-    text: 'Discover',
-    icon: <RiCompassDiscoverFill />,
-    route: '/dashboard/home'
-  },
-  { text: 'Calendar', icon: <FaCalendar />, route: '/dashboard/calendar' },
-  { text: 'Clubs', icon: <FaUsers />, route: '/dashboard/clubs' }
-];
+  const [isFaculty, setIsFaculty] = useState(false);
 
-const user = await checkUser();
-if ((user as User).isFaculty === true) {
-  buttonList.push({
-    text: 'Faculty Admin',
-    icon: <FaChalkboardTeacher />,
-    route: '/dashboard/assignFaculty'
-  });
-}
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const user = await checkUser();
+        if ((user as User).isFaculty) {
+          setIsFaculty(true);
+        }
+      } catch (error) {
+        console.error('Error checking user:', error);
+      }
+    };
 
-const Sidebar = () => (
-  <>
-    {buttonList.map((button) => (
-      <React.Fragment key={button.text}>
-        <SidebarButton
-          text={button.text}
-          icon={button.icon}
-          route={button.route}
-        />
-        <hr />
-      </React.Fragment>
-    ))}
-  </>
-);
+    fetchUserData();
+  }, []);
+
+  useEffect(() => {
+    if (isFaculty) {
+      setButtonList((prevList) => [
+        ...prevList,
+        {
+          text: 'Faculty Admin',
+          icon: <FaChalkboardTeacher />,
+          route: '/dashboard/assignFaculty',
+        },
+      ]);
+    }
+  }, [isFaculty]);
+
+  return (
+    <>
+      {buttonList.map((button) => (
+        <React.Fragment key={button.text}>
+          <SidebarButton
+            text={button.text}
+            icon={button.icon}
+            route={button.route}
+          />
+          <hr />
+        </React.Fragment>
+      ))}
+    </>
+  );
+};
 
 export default Sidebar;
