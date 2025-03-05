957640e055c6e0ae42a244dce253feda70a09956
cdrice26 <156816748+cdrice26@users.noreply.github.com>
2025-03-04 19:07:38 -0500

    Added query for user activity summary report

--- client/mcbb-frontend/src/reports.ts
+++ client/mcbb-frontend/src/reports.ts
@@ -1,6 +1,7 @@
 export interface Report {
   name: string;
   query: string;
+  queryParams: string[];
 }
 
 interface ReportObject {
@@ -11,7 +12,52 @@ interface ReportObject {
 }
 
 const REPORTS: ReportObject = {
-  SCHOOL_WIDE: [],
+  SCHOOL_WIDE: [
+    {
+      name: 'User Activity Summary',
+      query: /*sql*/`
+        WITH rsvp_counts AS (
+          SELECT 
+            user_id, 
+            COUNT(rsvp_id) AS events_rsvpd
+          FROM rsvp
+          WHERE is_active = 1
+            AND is_yes = 1
+          GROUP BY user_id
+        ),
+        subscription_counts AS (
+          SELECT 
+            email, 
+            COUNT(subscription_id) AS clubs_subscribed
+          FROM user_subscription
+          WHERE is_active = 1
+            AND subscribed_or_blocked = 1
+          GROUP BY email
+        ),
+        tag_groups AS (
+          SELECT 
+            ut.user_id, 
+            GROUP_CONCAT(t.tag_name SEPARATOR ', ') AS interests
+          FROM user_tags ut
+          JOIN tag t ON ut.tag_id = t.tag_id
+          GROUP BY ut.user_id
+        )
+        SELECT 
+          u.email, 
+          u.name, 
+          COALESCE(r.events_rsvpd, 0) AS events_rsvpd, 
+          COALESCE(us.clubs_subscribed, 0) AS clubs_subscribed,
+          COALESCE(t.interests, '') AS interests
+        FROM users u
+        LEFT JOIN rsvp_counts r ON u.email = r.user_id
+        LEFT JOIN subscription_counts us ON u.email = us.email
+        LEFT JOIN tag_groups t ON u.email = t.user_id
+        WHERE u.email = %s
+          AND u.school_id = %s
+        GROUP BY u.email, u.name;`,
+      queryParams: ['email', 'school_id']
+    }
+  ],
   CLUB: [],
   USER: [],
   EVENT: []
