eae51b8ec0895d9b96e59d0aab484e8d88fcd700
cdrice26 <156816748+cdrice26@users.noreply.github.com>
2025-06-21 09:45:33 -0400

    added pwa stuff

--- client/mcbb-frontend/vite.config.ts
+++ client/mcbb-frontend/vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'goSHARC: Student Happenings and Recommendations for Clubs',
+        short_name: 'goSHARC',
+        description:
+          'A centrailzed, online platform for university student engagement.',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/assets/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/assets/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      registerType: 'autoUpdate',
+      includeAssets: ['/assets/**/*']
+    })
+  ]
+});
